1c27a670afe785149c2c8692a75a1feac9a8a172
Addie Okoro <adinduokoro95@gmail.com>
2024-05-02 21:15:39 -0400

    Hero Section
    So the hero section is basically done. Only thing I would need to do is add the active states for the navigation menu.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import styles from "./Header.module.css"
-import logo from "../../assets/nile-style-logo.svg"
+import React from "react";
+import styles from "./Header.module.css";
+import logo from "../../assets/nile-style-logo.svg";
+import logoAnimation from "../../assets/since-1998.svg";
+import pageScroller from "../../assets/page-scroller.svg";
 
 const Header = () => {
   return (
     <div className={styles.header}>
-        <img className={styles.logo} src={logo} alt="logo" />
+      <img className={styles.logo} src={logo} alt="logo" />
+      <img
+        className={styles["logo-animation"]}
+        src={logoAnimation}
+        alt="since-1998"
+      />
+      <a href="">
+        <button className={styles.button}>
+          <p>Book an Appointment</p>
+        </button>
+      </a>
+      <img
+        className={styles["page-scroller"]}
+        src={pageScroller}
+        alt="page-scroller"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Hero.module.css";
 import { Header, Navigation } from "../../components";
+import logo from "../../assets/nile-style-logo.svg";
+import logoAnimation from "../../assets/since-1998.svg"
 
 const Hero = () => {
   return (
-    <section className={styles.box}>
+    <section className={styles.hero}>
       <Navigation />
       <Header />
     </section>
