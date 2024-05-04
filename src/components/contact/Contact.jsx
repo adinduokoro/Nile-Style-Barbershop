b4cbe9b28eae68d9c0a31e6e80272928e6214334
Addie Okoro <adinduokoro95@gmail.com>
2024-05-03 22:09:26 -0400

    Contact Section
    Almost have this section completed. Just need to finish the other half of the section.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-import styles from "./Contact.module.css"
-
+import React from "react";
+import styles from "./Contact.module.css";
+import { Button, PageTitle } from "../../components";
+import { hours } from "./data";
 
 const Contact = () => {
   return (
-    <section>Contact</section>
-  )
-}
+    <section className={styles.contact}>
+      <div className="wrapper">
+        <PageTitle
+          number={"5"}
+          title={"Contact"}
+          subTitle={"Visit Us Today!"}
+        />
+        <div className={styles["contact-content"]}>
+          <div className={styles["hours-of-operation"]}>
+            <h2 className={styles.title}>HOURS</h2>
+            <div className={styles["hours-container"]}>
+              {hours.map((hour, index) => {
+                return (
+                  <h2 className={styles.hour} key={index}>
+                    {hour.weekday} <span>{hour.hour}</span>
+                  </h2>
+                );
+              })}
+            </div>
+            <h4 className={styles.disclaimer}>
+              Last customer admitted 30 minutes prior to closing
+            </h4>
+          </div>
 
-export default Contact
+          {/* contact */}
+          
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/contact/data.jsx
@@ -0,0 +1,30 @@
+export const hours = [
+  {
+    weekday: "MONDAY",
+    hour: "CLOSED"
+  },
+  {
+    weekday: "TUESDAY",
+    hour: "8:00am - 6:00pm"
+  },
+  {
+    weekday: "WEDNESDAY",
+    hour: "8:00am - 6:00pm"
+  },
+  {
+    weekday: "THURSDAY",
+    hour: "8:00am - 6:00pm"
+  },
+  {
+    weekday: "FRIDAY",
+    hour: "8:00am - 6:00pm"
+  },
+  {
+    weekday: "SATURDAY",
+    hour: "7:00am - 5:00pm"
+  },
+  {
+    weekday: "SUNDAY",
+    hour: "CLOSED"
+  },
+]
