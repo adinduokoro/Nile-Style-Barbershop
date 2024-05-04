63eff19a6632d50797ed3b376d5f07646f9a4a6f
Addie Okoro <adinduokoro95@gmail.com>
2024-05-04 18:07:19 -0400

    Contact Section
    The contact section is completed other than a few things missing as far as in links for the social icons.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./Contact.module.css";
 import { Button, PageTitle } from "../../components";
 import { hours } from "./data";
+import facebookIcon from "../../assets/facebook-icon.svg";
+import instagramIcon from "../../assets/instagram-icon.svg";
 
 const Contact = () => {
   return (
@@ -12,7 +14,7 @@ const Contact = () => {
           title={"Contact"}
           subTitle={"Visit Us Today!"}
         />
-        <div className={styles["contact-content"]}>
+        <div className={styles["contact-container"]}>
           <div className={styles["hours-of-operation"]}>
             <h2 className={styles.title}>HOURS</h2>
             <div className={styles["hours-container"]}>
@@ -28,9 +30,29 @@ const Contact = () => {
               Last customer admitted 30 minutes prior to closing
             </h4>
           </div>
-
-          {/* contact */}
-          
+          <div className={styles["operation-info"]}>
+            <div className={styles.phone}>
+              <h2 className={styles.cta}>Get in Touch</h2>
+              <h2 className={styles.number}>(410) 624-5899</h2>
+            </div>
+            <div className={styles.address}>
+              <h2 className={styles.cta}>Location</h2>
+              <h2 className={styles["business-name"]}>Nile Style Barbershop</h2>
+              <h2 className={styles.street}>6622 Hardford Rd</h2>
+              <h2 className={styles.zipcode}>Baltimore MD, 21224</h2>
+            </div>
+            <div className={styles.social}>
+              <h2 className={styles.cta}>Follow Us</h2>
+              <div className={styles.icons}>
+                <a href="">
+                  <img src={facebookIcon} alt="" />
+                </a>
+                <a href="">
+                  <img src={instagramIcon} alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
