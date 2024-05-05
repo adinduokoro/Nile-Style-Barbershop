f08a3944d0159bbc34a0327ca0ba0b9877463081
Addie Okoro <adinduokoro95@gmail.com>
2024-05-05 09:26:09 -0400

    Home Page
    The homepage for the most part is complete and ready to go.

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Gallery.module.css";
 import { Button, PageTitle } from "../../components";
+import {images} from "./data"
 
 const Gallery = () => {
   return (
@@ -12,10 +13,15 @@ const Gallery = () => {
           subTitle={"That Won't Break the Bank"}
         />
         <div className={styles["gallery-content"]}>
-
+          {images.map((image, index) => {
+            return(
+              <div className={styles["image-container"]} key={index}>
+                <img src={image} alt="" />
+              </div>
+            )
+          })}
         </div>
         <Button text={"See More"} />
-
       </div>
     </section>
   );
---
+++ src/components/gallery/data.jsx
@@ -0,0 +1,8 @@
+import image1 from "../../assets/gallery-pic-1.jpg";
+import image2 from "../../assets/gallery-pic-2.jpg";
+import image3 from "../../assets/gallery-pic-3.jpg";
+import image4 from "../../assets/gallery-pic-4.jpg";
+import image5 from "../../assets/gallery-pic-5.jpg";
+import image6 from "../../assets/gallery-pic-6.jpg";
+
+export const images = [image1, image2, image3, image4, image5, image6];
